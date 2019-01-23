1677cedf3ac849daae456fab59bccf4d68b97129
Marcus Klaas <mail@marcusklaas.nl>
2019-01-23 12:30:19 +0100

    Implement ra_db mock's FileMap using sortedvec

--- crates/ra_db/src/mock.rs
+++ crates/ra_db/src/mock.rs
@@ -1,30 +1,31 @@
 use rustc_hash::FxHashSet;
-use relative_path::{RelativePath, RelativePathBuf};
+use sortedvec::def_sorted_vec;
+use relative_path::RelativePathBuf;
 
-use crate::{FileId};
+use crate::FileId;
 
-#[derive(Default, Debug, Clone)]
-pub struct FileMap(Vec<(FileId, RelativePathBuf)>);
+fn key_deriv(v: &(FileId, RelativePathBuf)) -> &str {
+    v.1.as_relative_path().as_str()
+}
+
+def_sorted_vec! {
+    #[derive(Debug, Clone)]
+    pub struct FileMap: (FileId, RelativePathBuf) => &str, key_deriv
+}
 
 impl FileMap {
     pub fn add(&mut self, path: RelativePathBuf) -> FileId {
-        let file_id = FileId((self.0.len() + 1) as u32);
-        self.0.push((file_id, path));
+        let file_id = FileId((self.len() + 1) as u32);
+        self.insert((file_id, path));
         file_id
     }
 
     pub fn files(&self) -> FxHashSet<FileId> {
-        self.iter().map(|(id, _)| id).collect()
+        self.iter().map(|&(id, _)| id).collect()
     }
 
     pub fn file_id(&self, path: &str) -> FileId {
         assert!(path.starts_with('/'));
-        self.iter().find(|(_, p)| p == &path[1..]).unwrap().0
-    }
-
-    fn iter<'a>(&'a self) -> impl Iterator<Item = (FileId, &'a RelativePath)> + 'a {
-        self.0
-            .iter()
-            .map(|(id, path)| (*id, path.as_relative_path()))
+        self.find(&&path[1..]).unwrap().0
     }
 }
