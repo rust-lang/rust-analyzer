9dad3ead630e152213a9a2d8d6d2134f12fa97b1
Wilco Kusee <wilcokusee@gmail.com>
2019-04-02 21:30:01 +0200

    Complete function body

--- crates/ra_ide_api/src/completion/complete_impl_fn.rs
+++ crates/ra_ide_api/src/completion/complete_impl_fn.rs
@@ -1,40 +1,71 @@
 use ra_syntax::{
     algo::{find_node_at_offset},
     ast::{self, FnDef, ImplItem, ImplItemKind, NameOwner},
-    AstNode, TreeArc, SmolStr
+    AstNode, TreeArc, SmolStr, SyntaxKind,
 };
 use hir::{Resolver, db::HirDatabase};
 
-use crate::completion::{CompletionContext, Completions, completion_item::{CompletionItem, CompletionKind}};
+use crate::completion::{
+    completion_item::{CompletionItem, CompletionKind, CompletionItemKind},
+    CompletionContext, Completions,
+};
 
 pub(super) fn complete_impl_fn(acc: &mut Completions, ctx: &CompletionContext) {
-    let impl_node = match find_node_at_offset::<ast::ImplBlock>(ctx.leaf, ctx.offset) {
+    let impl_node = match find_node_at_offset::<ast::ImplBlock>(ctx.token.parent(), ctx.offset) {
         Some(impl_node) => impl_node,
         None => return,
     };
-    let impl_item_list = impl_node.item_list().unwrap();
+    let impl_item_list = match impl_node.item_list() {
+        Some(impl_item_list) => impl_item_list,
+        None => return,
+    };
 
-    // TODO remove unwraps
-    let trait_def = resolve_target_trait_def(ctx.db, &ctx.resolver, impl_node).unwrap();
+    let trait_def = match resolve_target_trait_def(ctx.db, &ctx.resolver, impl_node) {
+        Some(trait_def) => trait_def,
+        None => return,
+    };
 
-    let trait_items =
-        trait_def.syntax().descendants().find_map(ast::ItemList::cast).unwrap().impl_items();
     let fn_def_opt = |kind| if let ImplItemKind::FnDef(def) = kind { Some(def) } else { None };
     let def_name = |def| -> Option<&SmolStr> { FnDef::name(def).map(ast::Name::text) };
 
-    let trait_fns = trait_items.map(ImplItem::kind).filter_map(fn_def_opt).collect::<Vec<_>>();
+    let trait_fns = {
+        let trait_items = match trait_def.syntax().descendants().find_map(ast::ItemList::cast) {
+            Some(item_list) => item_list.impl_items(),
+            None => return,
+        };
+        trait_items.map(ImplItem::kind).filter_map(fn_def_opt).collect::<Vec<_>>()
+    };
 
     let impl_items = impl_item_list.impl_items();
     let impl_fns = impl_items.map(ImplItem::kind).filter_map(fn_def_opt).collect::<Vec<_>>();
 
     trait_fns
         .into_iter()
-        .filter_map(|t| def_name(t))
-        .filter(|name| impl_fns.iter().all(|i| def_name(i) != Some(name)))
-        .for_each(|name| {
-            CompletionItem::new(CompletionKind::Reference, ctx.source_range(), name.to_string())
+        .filter(|t| def_name(t).is_some())
+        .filter(|t| impl_fns.iter().all(|i| def_name(i) != def_name(t)))
+        .map(|t| (def_name(t).unwrap(), build_func(t)))
+        .for_each(|(name, func)| {
+            CompletionItem::new(CompletionKind::Reference, ctx.source_range(), name.clone())
+                .kind(CompletionItemKind::Function)
+                .insert_snippet(func)
                 .add_to(acc)
         });
+}
+
+fn build_func(def: &ast::FnDef) -> String {
+    let mut buf = String::new();
+
+    for child in def.syntax().children() {
+        match (child.prev_sibling().map(|c| c.kind()), child.kind()) {
+            (_, SyntaxKind::SEMI) => buf.push_str(" { unimplemented!() }"),
+            (_, SyntaxKind::ATTR) | (_, SyntaxKind::COMMENT) => {}
+            (Some(SyntaxKind::ATTR), SyntaxKind::WHITESPACE)
+            | (Some(SyntaxKind::COMMENT), SyntaxKind::WHITESPACE) => {}
+            _ => child.text().push_to(&mut buf),
+        };
+    }
+
+    format!("fn {} {{ $0 }}", buf.trim_end())
 }
 
 // TODO de-duplicate from `add_missing_impl_members`
@@ -66,7 +97,7 @@ mod tests {
             "completes_trait_fn",
             r"
             trait T {
-                fn foo();
+                fn foo(a: u32) -> bool;
             }
 
             impl T for () {
