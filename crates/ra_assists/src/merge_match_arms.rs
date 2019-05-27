70b91f6fd37d0dbc514cb5a22d2f070cf786aaa7
Lenard Pratt <l3np27@gmail.com>
2019-05-27 14:33:04 +0100

    Ran cargo fmt

--- crates/ra_assists/src/merge_match_arms.rs
+++ crates/ra_assists/src/merge_match_arms.rs
@@ -1,8 +1,10 @@
 use hir::db::HirDatabase;
 use ra_syntax::{
-    ast::{MatchArm, AstNode,Pat},
+    ast::{MatchArm, AstNode,Pat,Whitespace,AstToken},
+    SyntaxElement,
     Direction,
     SyntaxKind::COMMA,
+    TextUnit,
     algo::non_trivia_sibling
 };
 
@@ -25,31 +27,59 @@ pub(crate) fn merge_match_arms(mut ctx: AssistCtx<impl HirDatabase>) -> Option<A
 
     let buf = build_buffer(match_arm.pats());
 
-    let comma = non_trivia_sibling( match_arm.syntax().into(), Direction::Next);
+    let comma = non_trivia_sibling(match_arm.syntax().into(), Direction::Next);
+
+    let white_space_offset = match_arm.syntax().prev_sibling_or_token();
 
     ctx.add_action(AssistId("merge_match_arms"), "merge match arms", |edit| {
-       
         edit.target(match_arm.syntax().range());
+
+        let offseting_amount = match white_space_offset {
+            Some(SyntaxElement::Token(tok)) => {
+                if let Some(_) = Whitespace::cast(tok) {
+                    let ele = white_space_offset.unwrap().range();
+                    ele.len()
+                } else {
+                    TextUnit::from(0)
+                }
+            }
+            _ => TextUnit::from(0),
+        };
 
         if let Some(comma) = comma {
             if comma.kind() == COMMA {
                 edit.delete(comma.range());
-            }   
+            }
         }
 
         edit.replace_node_and_indent(match_arm.syntax(), buf);
 
-        edit.set_cursor(next_arm.syntax().range().start())
-
+        if let Some(comma) = non_trivia_sibling(match_arm.syntax().into(), Direction::Next) {
+            if comma.kind() == COMMA {
+                edit.set_cursor(
+                    match_arm.syntax().range().end()
+                        + offseting_amount
+                        + comma.range().len()
+                        + TextUnit::from(2),
+                );
+            } else {
+                edit.set_cursor(
+                    match_arm.syntax().range().end() + offseting_amount + TextUnit::from(3),
+                )
+            }
+        } else {
+            println!("{} vs {}", match_arm.syntax().range().end(), next_arm.syntax().range().end());
+            edit.set_cursor(match_arm.syntax().range().end() + offseting_amount)
+        }
     });
 
     ctx.build()
 }
 
-fn build_buffer<'a>(pats:impl Iterator<Item = &'a Pat>) -> String {
+fn build_buffer<'a>(pats: impl Iterator<Item = &'a Pat>) -> String {
     let mut buf = String::new();
 
-   pats.for_each(|pat| buf.push_str(&format!("{}", pat.syntax().text())));
+    pats.for_each(|pat| buf.push_str(&format!("{}", pat.syntax().text())));
 
     buf.push_str(" |");
 
@@ -60,10 +90,34 @@ fn build_buffer<'a>(pats:impl Iterator<Item = &'a Pat>) -> String {
 mod tests {
     use super::*;
 
-    use crate::helpers::{check_assist};
+    use crate::helpers::{check_assist,check_assist_target};
 
     #[test]
-    fn merge_match_banches() {
+    fn merge_match_arms_target() {
+        check_assist_target(
+            merge_match_arms,
+            r#"
+            enum A {
+                Foo,
+                Bar,
+                Baz
+            }
+            fn f() {
+                let x = A::Foo;
+               
+                match x {
+                    Foo <|> => () 
+                    Bar => ()
+                    Baz => ()
+                }
+            }
+            "#,
+            r#"Foo  => ()"#,
+        );
+    }
+
+    #[test]
+    fn merge_match_arm() {
         check_assist(
             merge_match_arms,
             r#"
@@ -93,7 +147,7 @@ mod tests {
                
                 match x {
                     Foo |
-                    Bar => <|> (),
+                    Bar => <|>(),
                     Baz => (),
                 }
             }
@@ -102,7 +156,7 @@ mod tests {
     }
 
     #[test]
-    fn merge_match_banches_no_comma() {
+    fn merge_match_arms_no_comma() {
         check_assist(
             merge_match_arms,
             r#"
@@ -131,8 +185,8 @@ mod tests {
                 let x = A::Foo;
                
                 match x {
-                    Foo |
-                    Bar => <|> ()
+                    Foo | 
+                    Bar => <|>()
                     Baz => ()
                 }
             }
