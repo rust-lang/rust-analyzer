713a9a7f5b29900f6909302de001e1dc4ca7fba4
Devin R <devin.ragotzy@gmail.com>
2019-08-18 09:07:34 -0400

    build DiffView of scratch example

--- crates/ra_fmt2/src/fmt_diff.rs
+++ crates/ra_fmt2/src/fmt_diff.rs
@@ -16,8 +16,88 @@ use std::rc::Rc;
 #[derive(Debug, Clone)]
 pub(crate) struct DiffView {
     // TODO more diffing info not just a string
-    diffs: Vec<SmolStr>,
+    diffs: Vec<(usize, SmolStr)>,
     original: SmolStr,
+}
+
+impl DiffView {
+    fn apply(&mut self, block: &Block, rule: &SpacingRule) {
+        match rule.space.loc {
+            SpaceLoc::After => self.apply_after(block, rule.space.value),
+            SpaceLoc::Before => self.apply_before(block, rule.space.value),
+            SpaceLoc::Around => self.apply_around(block, rule.space.value),
+        }
+    }
+
+    fn apply_after(&mut self, block: &Block, value: SpaceValue) {
+        match value {
+            SpaceValue::Single => {
+                let after = block.text_range().end().to_usize();
+                self.diffs.push((after, SmolStr::from(" ")))
+            },
+            SpaceValue::Newline => {
+                let after = block.text_range().end().to_usize();
+                self.diffs.push((after, SmolStr::from("\n")))
+            },
+            SpaceValue::SingleOptionalNewline => {
+                let before = block.text_range().end().to_usize();
+
+                if !block.siblings_contain("\n") {
+                    println!("SIBLINGS CONTAIN FALSE");
+                    self.diffs.push((before, SmolStr::from(" ")))
+                } else {
+                    println!("SIBLINGS CONTAIN TRUE");
+                    self.diffs.push((before, SmolStr::from("\n")))
+                }
+            },
+            _ => {},
+        }
+    }
+
+    fn apply_before(&mut self, block: &Block, value: SpaceValue) {
+        match value {
+            SpaceValue::Single => {
+                let before = block.text_range().start().to_usize();
+                self.diffs.push((before, SmolStr::from(" ")))
+            },
+            SpaceValue::Newline => {
+                let before = block.text_range().start().to_usize();
+                self.diffs.push((before, SmolStr::from("\n")))
+            },
+            SpaceValue::SingleOptionalNewline => {
+                let before = block.text_range().start().to_usize();
+
+                if !block.siblings_contain("\n") {
+                    println!("SIBLINGS CONTAIN FALSE");
+                    self.diffs.push((before, SmolStr::from(" ")))
+                } else {
+                    println!("SIBLINGS CONTAIN TRUE");
+                    self.diffs.push((before, SmolStr::from("\n")))
+                }
+            },
+            _ => {},
+        }
+    }
+
+    fn apply_around(&mut self, block: &Block, value: SpaceValue) {
+        match value {
+            SpaceValue::Single => {
+                let before = block.text_range().start().to_usize();
+                let after = block.text_range().end().to_usize();
+
+                let mut pair = vec![(before, SmolStr::from(" ")),(after, SmolStr::from(" "))];
+                self.diffs.append(&mut pair)
+            },
+            SpaceValue::Newline => {
+                let before = block.text_range().start().to_usize();
+                let after = block.text_range().end().to_usize();
+                
+                let mut pair = vec![(before, SmolStr::from("\n")),(after, SmolStr::from("\n"))];
+                self.diffs.append(&mut pair)
+            },
+            _ => {},
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -35,31 +115,34 @@ impl FmtDiff {
         Self { edit_tree, diff }
     }
 
+    /// Checks if `Whitespace` and `SpacingRule` match then mutates `DiffView`.
+    /// 
+    /// # Arguments
+    ///
+    /// * `block` - A &Block that is always a token.
+    /// * `rule` - A &SpaceRule.
     fn check_spacing(&self, rule: &SpacingRule, block: &Block) {
         // refcell for here for mutating DiffView?
         if let Some(whitespace) = block.get_spacing() {
-            println!("WHITESPACE {:#?}", whitespace);
-            // is this a terible idea
+            // is this a terible idea impl-ing eq??
             if whitespace != rule {
-                self.diff.borrow_mut().update(rule)
+                self.diff.borrow_mut().apply(block, rule);
             }
         } else {
-            // total diff
+            self.diff.borrow_mut().apply(block, rule);
         }
     }
 
-    pub(crate) fn spacing_diff(&self) -> DiffView {
+    pub(crate) fn spacing_diff(self) -> DiffView {
         let space_rules = spacing();
         let spacing = PatternSet::new(space_rules.rules.iter());
 
         for block in self.edit_tree.walk() {
             for rule in spacing.matching(block.to_element()) {
-                println!("BLOCK {:#?}\nRULE {:?}", block, rule);
-
                 // creates DiffView
                 self.check_spacing(rule, block)
             }
         }
-        self.diff.clone().into_inner()
+        self.diff.into_inner()
     }
 }
