eed88dc74b9d8d1476435b61cfd7a3eb21b63b96
Devin R <devin.ragotzy@gmail.com>
2019-08-20 23:41:18 -0400

    all but ending new line works

--- crates/ra_fmt2/src/diff_view.rs
+++ crates/ra_fmt2/src/diff_view.rs
@@ -3,7 +3,7 @@ use crate::edit_tree::{Block, EditTree};
 use crate::pattern::{Pattern, PatternSet};
 use crate::rules::spacing;
 use crate::trav_util::{walk, walk_nodes, walk_tokens};
-use crate::whitespace::{Whitespace, WhitespaceAbstract};
+use crate::whitespace::{Whitespace};
 
 use ra_syntax::{
     NodeOrToken, SmolStr, SyntaxElement,
@@ -58,39 +58,40 @@ enum Edit {
 }
 
 impl Edit {
-    fn from_block(ws_abs: &dyn WhitespaceAbstract, text: &str, space: SpaceLoc) -> Self {
-        match space {
-            SpaceLoc::Before => {
-                // len of offending token
-                let len = ws_abs.prev_tkn_len() as isize;
-                let range = (len, 0);
+    // fn from_block(block: &Block, text: &str, space: SpaceLoc) -> Self {
+    //     match space {
+    //         SpaceLoc::Before => {
+    //             let w_space = block.get_spacing_mut();
+    //             // len of offending token
+    //             let len = block.prev_tkn_len() as isize;
+    //             let range = (len, 0);
 
-                let edit = (ws_abs.text_start() as isize, SmolStr::from(text));
+    //             let edit = (block.text_start() as isize, SmolStr::from(text));
                 
-                println!("len {:?} {:?}", range, ws_abs.text_range());
-                if (len > 1 && ws_abs.prev_is_whitespace()) || (!ws_abs.match_prev(text) && ws_abs.prev_is_whitespace()) {
-                    Edit::Space(SpaceEdit::Replace(edit, range))
-                } else {
-                    Edit::Space(SpaceEdit::Insert(edit))
-                }
-            },
-            SpaceLoc::After => {
-                // len of offending token
-                let len = ws_abs.next_tkn_len() as isize;
-                let range = (0, len);
+    //             println!("len {:?} {:?}", range, block.text_range());
+    //             if (len > 1 && block.prev_is_whitespace()) || (!block.match_prev(text) && block.prev_is_whitespace()) {
+    //                 Edit::Space(SpaceEdit::Replace(edit, range))
+    //             } else {
+    //                 Edit::Space(SpaceEdit::Insert(edit))
+    //             }
+    //         },
+    //         SpaceLoc::After => {
+    //             // len of offending token
+    //             let len = block.next_tkn_len() as isize;
+    //             let range = (0, len);
 
-                let edit = (ws_abs.text_end() as isize, SmolStr::from(text));
+    //             let edit = (block.text_end() as isize, SmolStr::from(text));
                 
-                println!("len {:?} {:?}", range, ws_abs.text_range());
-                if (len > 1 && ws_abs.next_is_whitespace()) || (!ws_abs.match_next(text) && ws_abs.next_is_whitespace()) {
-                    Edit::Space(SpaceEdit::Replace(edit, range))
-                } else {
-                    Edit::Space(SpaceEdit::Insert(edit))
-                }
-            },
-            _ => unreachable!("Edit::from_block should never be called with SpaceLoc::Around")
-        }
-    }
+    //             println!("len {:?} {:?}", range, block.text_range());
+    //             if (len > 1 && block.next_is_whitespace()) || (!block.match_next(text) && block.next_is_whitespace()) {
+    //                 Edit::Space(SpaceEdit::Replace(edit, range))
+    //             } else {
+    //                 Edit::Space(SpaceEdit::Insert(edit))
+    //             }
+    //         },
+    //         _ => unreachable!("Edit::from_block should never be called with SpaceLoc::Around")
+    //     }
+    // }
 }
 
 #[derive(Debug, Clone)]
@@ -108,115 +109,115 @@ impl DiffView {
         Self { ws_edits: BTreeSet::default(), original, }
     }
 
-    pub(crate) fn collect_space_edits(&mut self, ws_abs: &dyn WhitespaceAbstract, rule: &SpacingRule) {
-        match rule.space.loc {
-            SpaceLoc::After => self.collect_space_after(ws_abs, rule.space),
-            SpaceLoc::Before => self.collect_space_before(ws_abs, rule.space),
-            SpaceLoc::Around => self.collect_space_around(ws_abs, rule.space),
-        }
-    }
+//     pub(crate) fn collect_space_edits(&mut self, block: &Block, rule: &SpacingRule) {
+//         match rule.space.loc {
+//             SpaceLoc::After => self.fix_spacing_after(block, rule.space),
+//             SpaceLoc::Before => self.fix_spacing_before(block, rule.space),
+//             SpaceLoc::Around => self.fix_spacing_around(block, rule.space),
+//         }
+//     }
 
-    fn collect_space_after(&mut self, ws_abs: &dyn WhitespaceAbstract, space: Space) {
-        match space.value {
-            SpaceValue::Single => {
-                let edit = Edit::from_block(ws_abs, " ", space.loc);
-                self.ws_edits.insert(edit);
-            },
-            SpaceValue::Newline => {
-                let edit = Edit::from_block(ws_abs, "\n", space.loc);
-                self.ws_edits.insert(edit);
-;            },
-            SpaceValue::SingleOptionalNewline => {
-                if !ws_abs.siblings_contain("\n") {
-                    let edit = Edit::from_block(ws_abs, " ", space.loc);
-                    self.ws_edits.insert(edit);
-                } else {
-                    let edit = Edit::from_block(ws_abs, "\n", space.loc);
-                    self.ws_edits.insert(edit);
-                }
-            },
-            _ => {},
-        }
-    }
+//     fn fix_spacing_after(&mut self, block: &Block, space: Space) {
+//         match space.value {
+//             SpaceValue::Single => {
+//                 let edit = Edit::from_block(block, " ", space.loc);
+//                 self.ws_edits.insert(edit);
+//             },
+//             SpaceValue::Newline => {
+//                 let edit = Edit::from_block(block, "\n", space.loc);
+//                 self.ws_edits.insert(edit);
+// ;            },
+//             SpaceValue::SingleOptionalNewline => {
+//                 if !block.siblings_contain("\n") {
+//                     let edit = Edit::from_block(block, " ", space.loc);
+//                     self.ws_edits.insert(edit);
+//                 } else {
+//                     let edit = Edit::from_block(block, "\n", space.loc);
+//                     self.ws_edits.insert(edit);
+//                 }
+//             },
+//             _ => {},
+//         }
+//     }
 
-    fn collect_space_before(&mut self, ws_abs: &dyn WhitespaceAbstract, space: Space) {
-        match space.value {
-            SpaceValue::Single => {
-                let edit = Edit::from_block(ws_abs, " ", space.loc);
-                self.ws_edits.insert(edit);
-;            },
-            SpaceValue::Newline => {
-                let edit = Edit::from_block(ws_abs, "\n", space.loc);
-                self.ws_edits.insert(edit);
-            },
-            SpaceValue::SingleOptionalNewline => {
-                if !ws_abs.siblings_contain("\n") {
-                    let edit = Edit::from_block(ws_abs, " ", space.loc);
-                    self.ws_edits.insert(edit);
-                } else {
-                    let edit = Edit::from_block(ws_abs, "\n", space.loc);
-                    self.ws_edits.insert(edit);
-                }
-            },
-            _ => {},
-        }
-    }
+//     fn fix_spacing_before(&mut self, block: &Block, space: Space) {
+//         match space.value {
+//             SpaceValue::Single => {
+//                 let edit = Edit::from_block(block, " ", space.loc);
+//                 self.ws_edits.insert(edit);
+// ;            },
+//             SpaceValue::Newline => {
+//                 let edit = Edit::from_block(block, "\n", space.loc);
+//                 self.ws_edits.insert(edit);
+//             },
+//             SpaceValue::SingleOptionalNewline => {
+//                 if !block.siblings_contain("\n") {
+//                     let edit = Edit::from_block(block, " ", space.loc);
+//                     self.ws_edits.insert(edit);
+//                 } else {
+//                     let edit = Edit::from_block(block, "\n", space.loc);
+//                     self.ws_edits.insert(edit);
+//                 }
+//             },
+//             _ => {},
+//         }
+//     }
 
-    fn collect_space_around(&mut self, ws_abs: &dyn WhitespaceAbstract, space: Space) {
-        match space.value {
-            SpaceValue::Single => {
-                let pair = vec![
-                    Edit::from_block(ws_abs, " ", SpaceLoc::Before),
-                    Edit::from_block(ws_abs, " ", SpaceLoc::After),
-                ];
-                self.ws_edits.extend(pair)
-            },
-            SpaceValue::Newline => {
-                let pair = vec![
-                    Edit::from_block(ws_abs, "\n", SpaceLoc::Before),
-                    Edit::from_block(ws_abs, "\n", SpaceLoc::After),
-                ];
-                self.ws_edits.extend(pair)
-            },
-            _ => {},
-        }
-    }
+//     fn fix_spacing_around(&mut self, block: &Block, space: Space) {
+//         match space.value {
+//             SpaceValue::Single => {
+//                 let pair = vec![
+//                     Edit::from_block(block, " ", SpaceLoc::Before),
+//                     Edit::from_block(block, " ", SpaceLoc::After),
+//                 ];
+//                 self.ws_edits.extend(pair)
+//             },
+//             SpaceValue::Newline => {
+//                 let pair = vec![
+//                     Edit::from_block(block, "\n", SpaceLoc::Before),
+//                     Edit::from_block(block, "\n", SpaceLoc::After),
+//                 ];
+//                 self.ws_edits.extend(pair)
+//             },
+//             _ => {},
+//         }
+//     }
 
-    /// Apply the collected changes to text and return `Formatted`.
-    /// 
-    /// ?? Does it need to be result or are all errors handled in ra-syntax??
-    pub(crate) fn apply(&mut self) -> Result<String, ()> {
-        let mut fmt = self.original.as_bytes().to_vec();
-        let mut space_added = 0_isize;
-        for edit in self.ws_edits.iter() {
-            match edit {
-                Edit::Space(space) => {
-                    match space {
-                        SpaceEdit::Insert((mut pos, text)) => {
-                            pos += space_added;
+//     /// Apply the collected changes to text and return `Formatted`.
+//     /// 
+//     /// ?? Does it need to be result or are all errors handled in ra-syntax??
+//     pub(crate) fn apply_diff(&mut self) -> Result<String, ()> {
+//         let mut fmt = self.original.as_bytes().to_vec();
+//         let mut space_added = 0_isize;
+//         for edit in self.ws_edits.iter() {
+//             match edit {
+//                 Edit::Space(space) => {
+//                     match space {
+//                         SpaceEdit::Insert((mut pos, text)) => {
+//                             pos += space_added;
 
-                            // pos should never be negative it is the index of token in the SmolStr
-                            let idx = pos as usize;
-                            fmt.splice(idx..idx, text.as_bytes().iter().cloned());
+//                             // pos should never be negative it is the index of token in the SmolStr
+//                             let idx = pos as usize;
+//                             fmt.splice(idx..idx, text.as_bytes().iter().cloned());
 
-                            space_added += text.len() as isize;
-                        },
-                        SpaceEdit::Replace((mut pos, text), (remove, add)) => {
-                            pos += space_added;
-                            let start = (pos - remove) as usize;
-                            let end = (pos + add) as usize;
-                            fmt.splice(start..end, text.as_bytes().iter().cloned());
-                            //println!("POST SPLICE {} -= {} + {} - {}", space_added, remove, add, text.len());
-                            space_added -= remove + add - text.len() as isize;
-                        },
-                    }
+//                             space_added += text.len() as isize;
+//                         },
+//                         SpaceEdit::Replace((mut pos, text), (remove, add)) => {
+//                             pos += space_added;
+//                             let start = (pos - remove) as usize;
+//                             let end = (pos + add) as usize;
+//                             fmt.splice(start..end, text.as_bytes().iter().cloned());
+//                             //println!("POST SPLICE {} -= {} + {} - {}", space_added, remove, add, text.len());
+//                             space_added -= remove + add - text.len() as isize;
+//                         },
+//                     }
                     
-                },
-                Edit::Indent => unimplemented!("Get to work on Indents"),
-            }
-        }
-        Ok(String::from_utf8_lossy(&fmt).to_string())
-    }
+//                 },
+//                 Edit::Indent => unimplemented!("Get to work on Indents"),
+//             }
+//         }
+//         Ok(String::from_utf8_lossy(&fmt).to_string())
+//     }
 }
 
 
