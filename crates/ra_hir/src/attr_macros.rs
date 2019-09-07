ca9f8d3faa7651a615690ab86ff0110d2c0b943c
Evgenii P <eupn@protonmail.com>
2019-09-08 00:48:31 +0800

    Invert the condition in expand_attr_macro

--- crates/ra_hir/src/attr_macros.rs
+++ crates/ra_hir/src/attr_macros.rs
@@ -3,11 +3,10 @@ use ra_syntax::ast::{self, AstNode};
 mod derive;
 
 pub fn expand_attr_macro(attr_node: ast::Attr, target_node: ast::ModuleItem) -> Option<tt::Subtree> {
-    if !derive::is_derive_attr(&attr_node) {
-        log::warn!("Unimplemented macro attr: {}", attr_node.syntax().to_string());
-
-        return None;
-    } else {
-        derive::expand_derive_attr(attr_node, target_node)
+    if derive::is_derive_attr(&attr_node) {
+        return derive::expand_derive_attr(attr_node, target_node);
     }
+
+    log::warn!("Unimplemented macro attr: {}", attr_node.syntax().to_string());
+    None
 }
