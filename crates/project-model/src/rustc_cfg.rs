a3ecf2dc20020702b808443f286c7ca18c9a854b
KOLANICH <kolan_n@mail.ru>
2022-05-30 19:44:15 +0300

    Fixed some new usage of format incompatible with older versions of rustcintroduced in dbd5a70ea3b806c94fcbb37bb68decf63a9fe968.

--- crates/project-model/src/rustc_cfg.rs
+++ crates/project-model/src/rustc_cfg.rs
@@ -29,7 +29,7 @@ pub(crate) fn get(cargo_toml: Option<&ManifestPath>, target: Option<&str>) -> Ve
             );
             res.extend(rustc_cfgs.lines().filter_map(|it| it.parse().ok()));
         }
-        Err(e) => tracing::error!("failed to get rustc cfgs: {e:?}"),
+        Err(e) => tracing::error!("failed to get rustc cfgs: {:?}", e),
     }
 
     res
@@ -47,7 +47,7 @@ fn get_rust_cfgs(cargo_toml: Option<&ManifestPath>, target: Option<&str>) -> Res
         }
         match utf8_stdout(cargo_config) {
             Ok(it) => return Ok(it),
-            Err(e) => tracing::debug!("{e:?}: falling back to querying rustc for cfgs"),
+            Err(e) => tracing::debug!("{:?}: falling back to querying rustc for cfgs", e),
         }
     }
     // using unstable cargo features failed, fall back to using plain rustc
