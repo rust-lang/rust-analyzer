21e7d2eb37867a3b96a3429b49be1b2bc89071ed
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-04-25 00:37:39 +0530

    doc: de-indent custom snippets to fix output

--- crates/rust-analyzer/src/config.rs
+++ crates/rust-analyzer/src/config.rs
@@ -211,46 +211,46 @@ config_data! {
         /// Custom completion snippets.
         // NOTE: Keep this list in sync with the feature docs of user snippets.
         completion_snippets_custom: FxHashMap<String, SnippetDef> = r#"{
-            "Arc::new": {
-                "postfix": "arc",
-                "body": "Arc::new(${receiver})",
-                "requires": "std::sync::Arc",
-                "description": "Put the expression into an `Arc`",
-                "scope": "expr"
-            },
-            "Rc::new": {
-                "postfix": "rc",
-                "body": "Rc::new(${receiver})",
-                "requires": "std::rc::Rc",
-                "description": "Put the expression into an `Rc`",
-                "scope": "expr"
-            },
-            "Box::pin": {
-                "postfix": "pinbox",
-                "body": "Box::pin(${receiver})",
-                "requires": "std::boxed::Box",
-                "description": "Put the expression into a pinned `Box`",
-                "scope": "expr"
-            },
-            "Ok": {
-                "postfix": "ok",
-                "body": "Ok(${receiver})",
-                "description": "Wrap the expression in a `Result::Ok`",
-                "scope": "expr"
-            },
-            "Err": {
-                "postfix": "err",
-                "body": "Err(${receiver})",
-                "description": "Wrap the expression in a `Result::Err`",
-                "scope": "expr"
-            },
-            "Some": {
-                "postfix": "some",
-                "body": "Some(${receiver})",
-                "description": "Wrap the expression in an `Option::Some`",
-                "scope": "expr"
-            }
-        }"#,
+    "Arc::new": {
+        "postfix": "arc",
+        "body": "Arc::new(${receiver})",
+        "requires": "std::sync::Arc",
+        "description": "Put the expression into an `Arc`",
+        "scope": "expr"
+    },
+    "Rc::new": {
+        "postfix": "rc",
+        "body": "Rc::new(${receiver})",
+        "requires": "std::rc::Rc",
+        "description": "Put the expression into an `Rc`",
+        "scope": "expr"
+    },
+    "Box::pin": {
+        "postfix": "pinbox",
+        "body": "Box::pin(${receiver})",
+        "requires": "std::boxed::Box",
+        "description": "Put the expression into a pinned `Box`",
+        "scope": "expr"
+    },
+    "Ok": {
+        "postfix": "ok",
+        "body": "Ok(${receiver})",
+        "description": "Wrap the expression in a `Result::Ok`",
+        "scope": "expr"
+    },
+    "Err": {
+        "postfix": "err",
+        "body": "Err(${receiver})",
+        "description": "Wrap the expression in a `Result::Err`",
+        "scope": "expr"
+    },
+    "Some": {
+        "postfix": "some",
+        "body": "Some(${receiver})",
+        "description": "Wrap the expression in an `Option::Some`",
+        "scope": "expr"
+    }
+}"#,
 
         /// List of rust-analyzer diagnostics to disable.
         diagnostics_disabled: FxHashSet<String> = "[]",
