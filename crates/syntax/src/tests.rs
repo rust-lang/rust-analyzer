89398253a29e84fa2245cab23a6f5396d41035f8
Laurențiu Nicola <lnicola@dend.ro>
2023-05-09 07:15:16 +0300

    Update old parser benchmark

--- crates/syntax/src/tests.rs
+++ crates/syntax/src/tests.rs
@@ -39,7 +39,7 @@ fn benchmark_parser() {
         let _b = bench("parsing");
         let p = SourceFile::parse(&data);
         assert!(p.errors.is_empty());
-        assert_eq!(p.tree().syntax.text_range().len(), 352474.into());
+        assert_eq!(p.tree().syntax.text_range().len(), 352458.into());
         p.tree()
     };
 
