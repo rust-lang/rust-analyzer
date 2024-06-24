verus-lang/verus-analyzer

update verus parsing, expect ci failure

crates/parser/src/grammar/items.rs
@@ -469,6 +469,20 @@ fn fn_(p: &mut Parser<'_>, m: Marker) {
     // fn foo<T>() where T: Copy {}
     generic_params::opt_where_clause(p);
 
+    // Note: requires -> recommends -> ensures -> decreases 
+    if p.at(T![requires]) {
+        verus::requires(p);
+    }
+    if p.at(T![recommends]) {
+        verus::recommends(p);
+    }
+    if p.at(T![ensures]) {
+        verus::ensures(p);
+    }
+    if p.at(T![decreases]) {
+        verus::decreases(p);
+    }
+
     if p.at(T![;]) {
         // test fn_decl
         // trait T { fn foo(); }

rust-lang/rust-analyzer

crates/parser/src/grammar/items.rs
@@ -475,11 +475,24 @@ fn fn_(p: &mut Parser<'_>, m: Marker) {
     // fn foo<T>() where T: Copy {}
     generic_params::opt_where_clause(p);
 
-    // test fn_decl
-    // trait T { fn foo(); }
+    // Note: requires -> recommends -> ensures -> decreases
+    if p.eat(T![requires]) {
+        verus::requires(p);
+    }
+    if p.eat(T![recommends]) {
+        verus::recommends(p);
+    }
+    if p.eat(T![ensures]) {
+        verus::ensures(p);
+    }
+    if p.eat(T![decreases]) {
+        verus::decreases(p);
+    }
+
     if !p.eat(T![;]) {
         expressions::block_expr(p);
     }
+
     m.complete(p, FN);
 }
 