cbf6128b94868720771cab4869fed6ca97ba4ca4
Nicolas Guichard <nicolas.guichard@kdab.com>
2025-09-22 20:07:48 +0200

    Record inferred const for type or const placeholders
    This extends InferenceResult with a new const_of_const_placeholders map
    to record which const a given const or type placeholder gets resolved
    to.
    
    This is basically the same as type_of_type_placeholder, but for consts.
    
    Note that while surprising, TypeRef::Placeholder may indeed resolve to
    a const because except for arrays the parser doesn't know ahead of type
    if a _ will resolve to a type or a const.
    
    In the case of an array, we have an Expr::Underscore:
    ```
    let a: [_; _] = [4, 5, 6];
          //^ TypeRef::Placeholder that resolves to the type i32
             //^ Expr::Underscore that resolves to the const 3
    ```
    
    But for a struct we have a TypeRef::Placeholder:
    ```
    struct S<T, const N: usize>([T; N]);
    
    let s: S<_, _> = S([1, 2, 3]);
           //^ TypeRef::Placeholder that resolves to the type i32
              //^ TypeRef::Placeholder that resolves to the const 3
    ```

--- crates/hir-ty/src/infer.rs
+++ crates/hir-ty/src/infer.rs
@@ -47,7 +47,7 @@ use hir_def::{
     TupleFieldId, TupleId, VariantId,
     attrs::AttrFlags,
     expr_store::{Body, ExpressionStore, HygieneId, body::Param, path::Path},
-    hir::{BindingId, ExprId, ExprOrPatId, ExprOrPatIdPacked, LabelId, PatId},
+    hir::{BindingId, ExprId, ExprOrPatId, ExprOrPatIdPacked, LabelId, PatId, TypeRefIdOrConstRef},
     lang_item::LangItems,
     layout::Integer,
     resolver::{HasResolver, ResolveValueResult, Resolver, TypeNs, ValueNs},
@@ -99,8 +99,9 @@ use crate::{
     },
     method_resolution::CandidateId,
     next_solver::{
-        AliasTy, Const, ConstKind, DbInterner, ErrorGuaranteed, GenericArgs, Region, StoredFnSig,
-        StoredGenericArg, StoredGenericArgs, StoredTy, StoredTys, Term, Ty, TyKind, Tys,
+        AliasTy, Const, ConstKind, DbInterner, ErrorGuaranteed, GenericArgs, Region, StoredConst,
+        StoredFnSig, StoredGenericArg, StoredGenericArgs, StoredTy, StoredTys, Term, Ty, TyKind,
+        Tys,
         abi::Safety,
         infer::{InferCtxt, ObligationInspector, traits::ObligationCause},
     },
@@ -775,6 +776,7 @@ pub struct InferenceResult<'db> {
     pub(crate) type_of_pat: ArenaMap<PatId, StoredTy>,
     pub(crate) type_of_binding: ArenaMap<BindingId, StoredTy>,
     pub(crate) type_of_type_placeholder: FxHashMap<TypeRefId, StoredTy>,
+    pub(crate) const_of_const_placeholder: FxHashMap<TypeRefIdOrConstRef, StoredConst>,
     pub(crate) type_of_opaque: FxHashMap<InternedOpaqueTyId<'db>, StoredTy>,
 
     /// Whether there are any type-mismatching errors in the result.
@@ -1122,6 +1124,7 @@ impl<'db> InferenceResult<'db> {
             type_of_pat: Default::default(),
             type_of_binding: Default::default(),
             type_of_type_placeholder: Default::default(),
+            const_of_const_placeholder: Default::default(),
             type_of_opaque: Default::default(),
             skipped_ref_pats: Default::default(),
             has_errors: Default::default(),
@@ -1196,6 +1199,14 @@ impl<'db> InferenceResult<'db> {
     }
     pub fn type_of_type_placeholder<'a>(&self, type_ref: TypeRefId) -> Option<Ty<'a>> {
         self.type_of_type_placeholder.get(&type_ref).map(|ty| ty.as_ref())
+    }
+    pub fn placeholder_consts<'a>(&self) -> impl Iterator<Item = (TypeRefIdOrConstRef, Const<'a>)> {
+        self.const_of_const_placeholder
+            .iter()
+            .map(|(&type_ref_or_const, const_)| (type_ref_or_const, const_.as_ref()))
+    }
+    pub fn const_of_const_placeholder<'a>(&self, expr: TypeRefIdOrConstRef) -> Option<Const<'a>> {
+        self.const_of_const_placeholder.get(&expr).map(|ty| ty.as_ref())
     }
     pub fn type_of_expr_or_pat<'a>(&self, id: ExprOrPatId) -> Option<Ty<'a>> {
         match id {
@@ -1483,6 +1494,7 @@ impl<'db> InferenceContext<'db> {
             type_of_pat,
             type_of_binding,
             type_of_type_placeholder,
+            const_of_const_placeholder,
             type_of_opaque,
             has_errors: _,
             diagnostics: _,
@@ -1514,6 +1526,7 @@ impl<'db> InferenceContext<'db> {
         merge_arena_maps(type_of_pat, &other.type_of_pat);
         merge_arena_maps(type_of_binding, &other.type_of_binding);
         merge_hash_maps(type_of_type_placeholder, &other.type_of_type_placeholder);
+        merge_hash_maps(const_of_const_placeholder, &other.const_of_const_placeholder);
         merge_hash_maps(type_of_opaque, &other.type_of_opaque);
         merge_hash_maps(expr_adjustments, &other.expr_adjustments);
         merge_hash_maps(pat_adjustments, &other.pat_adjustments);
@@ -1649,6 +1662,7 @@ impl<'db> InferenceContext<'db> {
             type_of_pat,
             type_of_binding,
             type_of_type_placeholder,
+            const_of_const_placeholder,
             type_of_opaque,
             skipped_ref_pats,
             closures_data,
@@ -1687,6 +1701,10 @@ impl<'db> InferenceContext<'db> {
             resolver.resolve_completely(ty);
         }
         type_of_type_placeholder.shrink_to_fit();
+        for const_ in const_of_const_placeholder.values_mut() {
+            resolver.resolve_completely(const_);
+        }
+        const_of_const_placeholder.shrink_to_fit();
         type_of_opaque.shrink_to_fit();
 
         if let Some(nodes_with_type_mismatches) = nodes_with_type_mismatches {
@@ -1993,6 +2011,7 @@ impl<'db> InferenceContext<'db> {
             InferenceTyDiagnosticSource::Body => Some(&mut InferenceTyLoweringVarsCtx {
                 table: &mut self.table,
                 type_of_type_placeholder: &mut self.result.type_of_type_placeholder,
+                const_of_const_placeholder: &mut self.result.const_of_const_placeholder,
             } as _),
             InferenceTyDiagnosticSource::Signature => None,
         };
@@ -2335,6 +2354,7 @@ impl<'db> InferenceContext<'db> {
         let mut vars_ctx = InferenceTyLoweringVarsCtx {
             table: &mut self.table,
             type_of_type_placeholder: &mut self.result.type_of_type_placeholder,
+            const_of_const_placeholder: &mut self.result.const_of_const_placeholder,
         };
         let mut ctx = TyLoweringContext::new(
             self.db,
