5c827dbf262371136e8662f3de48d925237c8868
A4-Tacks <wdsjxhno1001@163.com>
2025-10-28 13:22:07 +0800

    Add ide-assist: convert_to_format_string
    Example
    ---
    ```rust
    fn foo() {
        let n = 2;
        let s = "n: {n$0}";
    }
    ```
    ->
    ```rust
    fn foo() {
        let n = 2;
        let s = format!("n: {n}");
    }
    ```

--- crates/ide-assists/src/tests/generated.rs
+++ crates/ide-assists/src/tests/generated.rs
@@ -732,6 +732,25 @@ fn main() {
 }
 
 #[test]
+fn doctest_convert_to_format_string() {
+    check_doc_test(
+        "convert_to_format_string",
+        r#####"
+fn foo() {
+    let n = 2;
+    let s = "n: {n$0}";
+}
+"#####,
+        r#####"
+fn foo() {
+    let n = 2;
+    let s = format!("n: {n}");
+}
+"#####,
+    )
+}
+
+#[test]
 fn doctest_convert_to_guarded_return() {
     check_doc_test(
         "convert_to_guarded_return",
