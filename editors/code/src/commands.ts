fc20b39f8545d3e4fc63ef91524dc4e5dde0f513
ERoydev <e.roydev@gmail.com>
2025-11-06 11:43:44 +0200

    add: Add function to fetch runnable at cursor position

--- editors/code/src/commands.ts
+++ editors/code/src/commands.ts
@@ -14,6 +14,7 @@ import {
     selectRunnable,
     createTaskFromRunnable,
     createCargoArgs,
+    selectRunnableAtCursor,
 } from "./run";
 import {
     isRustDocument,
@@ -1209,6 +1210,13 @@ export function debug(ctx: CtxInit): Cmd {
 export function debugSingle(ctx: CtxInit): Cmd {
     return async (config: ra.Runnable) => {
         await startDebugSession(ctx, config);
+    };
+}
+
+export function fetchRunnableAtCursor(ctx: CtxInit): Cmd {
+    return async () => {
+        const editor = ctx.activeRustEditor ?? ctx.activeCargoTomlEditor;
+        return await selectRunnableAtCursor(ctx, editor, undefined);
     };
 }
 
