d188383047b76de37f06be6d2db693a0462c3c68
bit-aloo <sshourya17@gmail.com>
2025-11-07 23:58:55 +0530

    add postcard related methods to proc-macro-apo

--- crates/proc-macro-srv-cli/src/main_loop.rs
+++ crates/proc-macro-srv-cli/src/main_loop.rs
@@ -197,8 +197,6 @@ fn run_postcard() -> io::Result<()> {
             thread::sleep(std::time::Duration::from_secs(1));
             continue;
         };
-        dbg!(&req);
-
         let res = match req {
             msg::Request::ListMacros { dylib_path } => {
                 msg::Response::ListMacros(srv.list_macros(&dylib_path).map(|macros| {
@@ -301,7 +299,6 @@ fn run_postcard() -> io::Result<()> {
             }
         };
 
-        dbg!(&res);
         let res = postcard_wire::encode_cobs(&res).unwrap();
         postcard_wire::write_postcard(&mut writer, &res)?;
     }
